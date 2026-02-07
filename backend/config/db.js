de0d32eaaf1d83c7c4acd97a07e7e5cbc1db6b30
Daya Sagar <152199702+Dayasagar88@users.noreply.github.com>
2026-02-07 22:07:26 +0530

    first commit

---
+++ backend/config/db.js
@@ -0,0 +1,20 @@
+import dns from "dns";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+// Optional DNS fix
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+const connectDb = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL);
+    console.log("DB connected");
+  } catch (error) {
+    console.error("DB connection error:", error.message);
+    process.exit(1);
+  }
+};
+
+export default connectDb;
